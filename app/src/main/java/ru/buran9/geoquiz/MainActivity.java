c743ca79b15db893cb47b47bcfb67deeb95c4372
buran9 <meovoto@rambler.ru>
2022-01-20 16:00:56 +0300

    last modify on notebook

--- app/src/main/java/ru/buran9/geoquiz/MainActivity.java
+++ app/src/main/java/ru/buran9/geoquiz/MainActivity.java
@@ -6,37 +6,80 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mainTrueButton;
     private Button mainFalseButton;
+    private Button mainNextButton;
+    private TextView mainQuestionTextview;
+    private int questionCurrentIndex =0;
+    List<Question> questionBank = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        questionBank.add(new Question(R.string.question_1, true));
+        questionBank.add(new Question(R.string.question_2, true));
+        questionBank.add(new Question(R.string.question_3, false));
+        questionBank.add(new Question(R.string.question_4, false));
+        questionBank.add(new Question(R.string.question_5, true));
+
         mainFalseButton = (Button) findViewById(R.id.button_main_false);
         mainTrueButton = (Button) findViewById(R.id.button_main_true);
+        mainNextButton = (Button) findViewById(R.id.button_main_next);
+        mainQuestionTextview = (TextView) findViewById(R.id.textview_main_question);
+        mainQuestionTextview.setText(questionBank.get(questionCurrentIndex).getResTextId());
+
+        mainNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                questionCurrentIndex = (questionCurrentIndex + 1) % questionBank.size();
+                updateQuestion();
+            }
+        });
 
         mainTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(MainActivity.this, R.string.toast_correct, Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.TOP, 0, 0);
-                toast.show();
+            public void onClick(View view) {
+                checkAnswer(true);
             }
         });
 
         mainFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, R.string.toast_incorrect, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+    }
+
+    private void updateQuestion() {
+        int questionId = questionBank.get(questionCurrentIndex).getResTextId();
+        mainQuestionTextview.setText(questionId);
+    }
+
+    private void checkAnswer(boolean userPressTrue) {
+        boolean answerTrueFalse = questionBank.get(questionCurrentIndex).isAnswerTrueFalse();
+
+        int msgResId = 0;
+        if (userPressTrue == answerTrueFalse) {
+            msgResId = R.string.toast_correct;
+        } else {
+            msgResId = R.string.toast_incorrect;
+        }
+
+        Toast.makeText(MainActivity.this, msgResId, Toast.LENGTH_SHORT).show();
 
     }
 }
